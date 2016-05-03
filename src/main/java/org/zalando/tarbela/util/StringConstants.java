0131f1c8941783ecd6c43c0ed67a20c11647b1e1
Paul Ebermann <paul.ebermann@zalando.de>
2016-05-03 18:01:18 +0200

    Issue #7: fix setup issues.
    - fixed media type name
    - inline DeliveryStatus in API
    - add local producer URI

--- src/main/java/org/zalando/tarbela/util/StringConstants.java
+++ src/main/java/org/zalando/tarbela/util/StringConstants.java
@@ -2,6 +2,6 @@ package org.zalando.tarbela.util;
 
 public class StringConstants {
     public static final String CONTENT_TYPE_PROBLEM_JSON = "application/problem+json";
-    public static final String CONTENT_TYPE_BUNCH_OF_EVENTS = "application/x.tarbela.events-list";
+    public static final String CONTENT_TYPE_BUNCH_OF_EVENTS = "application/x.tarbela.event-list+json";
 
 }
