e0a6b0a2efb448c6d516352354bd34d186b01b47
Paul Ebermann <paul.ebermann@zalando.de>
2016-05-09 13:37:42 +0200

    Issue #7: use StupsOAuth2RestTemplate directly.
    The autowiring with `@OAuth2RestOperationsAutowired` didn't use our request factory.

--- src/main/java/org/zalando/tarbela/config/NakadiConfiguration.java
+++ src/main/java/org/zalando/tarbela/config/NakadiConfiguration.java
@@ -1,13 +1,18 @@
 package org.zalando.tarbela.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
 import org.springframework.web.client.RestTemplate;
 
-import org.zalando.stups.tokens.annotation.OAuth2RestOperationsAutowired;
+import org.zalando.stups.oauth2.spring.client.StupsOAuth2RestTemplate;
+import org.zalando.stups.oauth2.spring.client.StupsTokensAccessTokenProvider;
+import org.zalando.stups.tokens.AccessTokens;
 
 import org.zalando.tarbela.nakadi.NakadiClient;
 import org.zalando.tarbela.nakadi.NakadiClientImpl;
@@ -16,14 +21,21 @@ import org.zalando.tarbela.nakadi.NakadiClientImpl;
 public class NakadiConfiguration {
     private static final String NAKADI_TOKEN_NAME = "zalando-nakadi";
 
-    @OAuth2RestOperationsAutowired(NAKADI_TOKEN_NAME)
-    private RestTemplate template;
+    @Autowired
+    private HttpComponentsClientHttpRequestFactory requestFactory;
+
+    @Autowired
+    private AccessTokens accessTokens;
 
     @Value("${NAKADI_SUBMISSION_URI}")
     private String nakadiURI;
 
     @Bean
     public NakadiClient nakadiClient() {
-        return new NakadiClientImpl(nakadiURI, template);
+        return new NakadiClientImpl(nakadiURI, createTemplate(NAKADI_TOKEN_NAME));
+    }
+
+    private RestTemplate createTemplate(final String tokenName) {
+        return new StupsOAuth2RestTemplate(new StupsTokensAccessTokenProvider(tokenName, accessTokens), requestFactory);
     }
 }
