fbd0b77d8a918a25aa20094c32c3bfb35c1c8ae3
Paul Ebermann <paul.ebermann@zalando.de>
2016-05-17 15:07:28 +0200

    Issue #7: add happy case test for EventService.
    This currently fails, because we are not batching the events properly (i.e. Nakadi gets called twice, the producer too).

--- src/main/java/org/zalando/tarbela/util/ZipUtils.java
+++ src/main/java/org/zalando/tarbela/util/ZipUtils.java
@@ -1,6 +1,6 @@
 package org.zalando.tarbela.util;
 
-import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newArrayListWithCapacity;
 
 import java.util.Iterator;
 import java.util.List;
@@ -14,6 +14,16 @@ import com.google.common.base.Preconditions;
  */
 public class ZipUtils {
 
+    /**
+     * Iterates two iterables in parallel, passing each pair of items to a BiConsumer. This will stop as soon as one of
+     * the iterables runs out of elements.
+     *
+     * @param   ct    the first iterable.
+     * @param   cu    the second iterable.
+     * @param   each  the consumer to call for each pair of items.
+     *
+     * @return  {@code true} if both iterables are exhausted, {@code false} if one of them has still items in it.
+     */
     public static <T, U> boolean forEachPair(final Iterable<T> ct, final Iterable<U> cu, final BiConsumer<T, U> each) {
         final Iterator<T> it = ct.iterator();
         final Iterator<U> iu = cu.iterator();
@@ -25,9 +35,9 @@ public class ZipUtils {
     }
 
     /**
-     * Maps pairs of a entries from each list into a new list. This happens in a non-lazy way (i.e. a fully constructed
-     * list is returned). Both argument lists need to have the same size, which will also be the size of the returned
-     * list.
+     * Maps pairs of a entries from each list into a new (mutable) list. This happens in a non-lazy way (i.e. a fully
+     * constructed list is returned), changes on the result list will not reflect back on the original list. Both
+     * argument lists need to have the same size, which will also be the size of the returned list.
      *
      * @param   tList    first argument list
      * @param   uList    second argument list
@@ -41,7 +51,7 @@ public class ZipUtils {
             "argument lists don't have the same size! (tList.size = {} <> uList.size = {})", tList.size(),
             uList.size());
 
-        final List<R> result = newArrayList();
+        final List<R> result = newArrayListWithCapacity(tList.size());
         forEachPair(tList, uList, (t, u) -> result.add(mapping.apply(t, u)));
         return result;
     }
---
+++ src/test/java/org/zalando/tarbela/service/EventServiceTest.java
@@ -0,0 +1,135 @@
+package org.zalando.tarbela.service;
+
+import static org.junit.Assert.assertThat;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.zalando.tarbela.nakadi.NakadiClient;
+import org.zalando.tarbela.nakadi.NakadiResponseCallback;
+import org.zalando.tarbela.producer.EventRetriever;
+import org.zalando.tarbela.producer.EventStatusUpdater;
+import org.zalando.tarbela.producer.EventsWithNextPage;
+import org.zalando.tarbela.producer.models.Event;
+import org.zalando.tarbela.producer.models.EventChannel;
+import org.zalando.tarbela.producer.models.EventUpdate;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+public class EventServiceTest {
+
+    private static final String EVENT_TYPE_1 = "exampleHappened";
+    private static final String EVENT_TYPE_2 = "DataChanged";
+
+    @Mock
+    private EventRetriever retriever;
+    @Mock
+    private NakadiClient nakadiClient;
+    @Mock
+    private EventStatusUpdater updater;
+    @Mock
+    private EventsWithNextPage firstPage;
+    @Mock
+    private EventsWithNextPage secondPage;
+
+    @Captor
+    private ArgumentCaptor<NakadiResponseCallback> callbackCaptor;
+    @Captor
+    private ArgumentCaptor<List<Map<String, Object>>> nakadiEventsCaptor;
+    @Captor
+    private ArgumentCaptor<List<EventUpdate>> updatesCaptor;
+
+    @InjectMocks
+    private EventService service = new EventServiceImpl();
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(retriever.retrieveEvents()).thenReturn(firstPage);
+
+    }
+
+    @Test
+    public void testHappyCase() {
+        final ImmutableMap<String, Object> payLoad1 = ImmutableMap.of("hello", "World");
+        final ImmutableMap<String, Object> payLoad2 = ImmutableMap.of("hello", "other world");
+        when(firstPage.getEvents()).thenReturn(ImmutableList.of(makeEvent(EVENT_TYPE_1, "1", payLoad1),
+                makeEvent(EVENT_TYPE_1, "2", payLoad2)));
+        when(firstPage.nextPage()).thenReturn(Optional.empty());
+
+        doAnswer(invocation -> {
+                                    callbackCaptor.getValue().successfullyPublished();
+                                    return null;
+                                }).when(nakadiClient).submitEvents(anyString(), nakadiEventsCaptor.capture(),
+                                    callbackCaptor.capture());
+
+        doNothing().when(updater).updateStatuses(updatesCaptor.capture());
+
+        service.publishEvents();
+
+        verify(nakadiClient).submitEvents(eq(EVENT_TYPE_1), eq(ImmutableList.of(payLoad1, payLoad2)),
+            any(NakadiResponseCallback.class));
+
+        assertThat(updatesCaptor.getValue(),
+            containsInAnyOrder(updateWithIdAndStatus("1", "SENT"), updateWithIdAndStatus("2", "SENT")));
+    }
+
+    private Event makeEvent(final String eventType, final String eId, final Map<String, Object> payload) {
+        final EventChannel channel = new EventChannel();
+        channel.setTopicName(eventType);
+        channel.setSinkIdentifier("zalando-nakadi");
+
+        final Event e = new Event();
+        e.setChannel(channel);
+        e.setEventId(eId);
+        e.setEventPayload(new HashMap<>(payload));
+        return e;
+    }
+
+    @SafeVarargs
+    private static <X> Matcher<Iterable<? extends X>> containsInAnyOrder(final Matcher<X>... matchers) {
+        return Matchers.containsInAnyOrder(matchers);
+    }
+
+    private Matcher<EventUpdate> updateWithIdAndStatus(final String id, final String status) {
+        return new TypeSafeMatcher<EventUpdate>() {
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("EventUpdate( eid=" + id + ", status=" + status + ")");
+            }
+
+            @Override
+            protected boolean matchesSafely(final EventUpdate item) {
+                return item.getEventId().equals(id) && item.getDeliveryStatus().equals(status);
+            }
+        };
+    }
+
+}
