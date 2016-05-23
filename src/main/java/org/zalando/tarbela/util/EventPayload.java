5c468b9f71449269e61520f6cc14b4a1b6f4bf29
Manuel Kunz <manuel.kunz@zalando.de>
2016-05-23 10:10:36 +0200

    Issue #21: switched to import-mappings property

--- src/main/java/org/zalando/tarbela/util/EventPayload.java
+++ src/main/java/org/zalando/tarbela/util/EventPayload.java
@@ -3,6 +3,6 @@ package org.zalando.tarbela.util;
 import java.util.HashMap;
 
 /**
- * Workaround for enabling type mapping from EventPayload to HashMap in swagger-code-gen-maven-plugin.
+ * Workaround for enabling type mapping from EventPayload to HashMap in swagger-codegen-maven-plugin.
  */
 public class EventPayload extends HashMap<String, Object> { }
